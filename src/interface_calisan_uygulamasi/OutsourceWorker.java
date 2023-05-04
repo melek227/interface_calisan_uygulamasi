19f075bb1657dcc9dbacf1a10572d1df783ec422
melek277 <melek_yilmaz_1997@hotmail.com>
2023-05-04 11:58:02 +0300

    Initial commit

---
+++ src/interface_calisan_uygulamasi/OutsourceWorker.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interface_calisan_uygulamasi;
+
+/**
+ *
+ * @author melek
+ */
+public class OutsourceWorker implements IWorkable{
+
+    @Override
+    public void work() {
+    }
+    
+    
+}
---
+++ src/interface_calisan_uygulamasi/Worker.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interface_calisan_uygulamasi;
+
+/**
+ *
+ * @author melek
+ */
+public class Worker implements IPayable, IEatable,IWorkable {
+
+    @Override
+    public void pay() {
+    }
+
+    @Override
+    public void eat() {
+    }
+
+    @Override
+    public void work() {
+    }
+    
+    
+    
+}
